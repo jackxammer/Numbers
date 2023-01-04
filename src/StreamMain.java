b52c8c17e32244944c070ba87df36a44af5afe1d
Evgeniy Baev <jackxammer@gmail.com>
2023-01-04 22:40:56 +0300

    updates in StreamMain

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -4,12 +4,11 @@ import java.util.stream.Stream;
 
 public class StreamMain {
 
-    public static StreamMain apiRealisation(List<Integer> intList) {
+    public static void apiRealisation(List<Integer> intList) {
         Stream<Integer> stream = intList.stream()
                 .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
                 .sorted(Comparator.naturalOrder());
         stream.forEach(System.out::println);
-        return null;
     }
 }
